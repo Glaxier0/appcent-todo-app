906ef82c3820dc4db233ca517a655fb0a07cfecb
glaxier0 <tahir2001@hotmail.com>
2022-04-30 23:20:40 +0300

    Added swagger config

---
+++ src/main/java/com/glaxier/todo/config/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package com.glaxier.todo.config;
+
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("To-Do App REST API")
+                        .description("Appcent To-Do App REST API by Tahir Çelik")
+                        .version("1.0")
+                        .license(new License().name("MIT License")
+                                .url("https://github.com/Glaxier0/appcent-todo-app/blob/main/LICENSE")));
+    }
+}
